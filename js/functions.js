9d3292b9d86b7094a116d5d4df2ac98897432279
Neil Hemnani <hemnanineil@gmail.com>
2020-12-30 00:08:47 -0500

    refractored JS code to make everything cleaner, and put functions in a separate file for ease of use

---
+++ js/functions.js
@@ -0,0 +1,18 @@
+export function initSketchArea(n=16) {
+    const sketchArea = document.querySelector('#sketch-area');
+    while (sketchArea.firstChild)
+        sketchArea.removeChild(sketchArea.firstChild);
+
+    sketchArea.style['grid-template-columns'] = `repeat(${n}, 1fr)`;
+    
+    const squarePadding = (320 / n) - 1;
+    for (let i = 0; i < n*n; i++) {
+        const square = document.createElement('div');
+        square.setAttribute('class', 'square');
+        square.style.padding = `${squarePadding}px`;
+        sketchArea.appendChild(square);
+    }
+    
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => square.addEventListener('mouseover', () => square.style.backgroundColor = '#000000'));
+}
--- js/scripts.js
+++ js/scripts.js
@@ -1,48 +1,11 @@
-const sketchArea = document.querySelector('#sketch-area');
-let n = 16;
-
-function initSketchArea() {
-    while (sketchArea.firstChild)
-        sketchArea.removeChild(sketchArea.firstChild);
-
-    sketchArea.style['grid-template-columns'] = `repeat(${n}, 1fr)`;
-    
-    const squarePadding = (320 / n) - 1;
-    for (let i = 0; i < n*n; i++) {
-        const square = document.createElement('div');
-        square.setAttribute('class', 'square');
-        square.style.padding = `${squarePadding}px`;
-        sketchArea.appendChild(square);
-    }
-    
-    const squares = document.querySelectorAll('.square');
-    squares.forEach(square => square.addEventListener('mouseover', squareHover));
-}
+import { initSketchArea } from './functions.js';
 
 initSketchArea();
 
-function squareHover(e) {
-    console.log(e.target.id);
-    e.target.style.backgroundColor = '#000000';
-}
+const slider = document.querySelector("#myRange");
+const output = document.querySelector("#demo");
+slider.addEventListener('mousemove', () => output.innerHTML = slider.value);
+slider.addEventListener('mouseup', () => initSketchArea(slider.value));
 
-function clear() {
-    if (n) {
-        initSketchArea(n);
-     } else {
-         n = 16;
-         initSketchArea();
-     }
-}
-
-clearBtn = document.querySelector('#clearbtn');
-clearBtn.addEventListener('click', () => clear());
-
-let slider = document.getElementById("myRange");
-let output = document.getElementById("demo");
-output.innerHTML = slider.value;
-slider.addEventListener('mousemove', () => {
-    n = slider.value;
-    output.innerHTML = slider.value;
-});
-slider.addEventListener('mouseup', () => initSketchArea());
+const clearBtn = document.querySelector('#clearbtn');
+clearBtn.addEventListener('click', () => initSketchArea(slider.value))
